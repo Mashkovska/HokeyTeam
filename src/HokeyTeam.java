00827ca30bd2f82a09c2fb3cb70a623c883b51b5
Mashkovska <32987039+Mashkovska@users.noreply.github.com>
2018-05-31 22:41:52 +0300

    Add files via upload
    checked code

--- src/HokeyTeam.java
+++ src/HokeyTeam.java
@@ -1,81 +1,83 @@
 
 public class HokeyTeam {
-	
-	private String name;
-	private String surname;
-	private String teamName;
-	private int numberOfmember;
-	private int Members;
-	public static int totalMembers;
 
-	HokeyTeam() {}
-	
-	public HokeyTeam(String name,String surname, String teamName, int numberOfmember ) {
-		this.name = name;
-		this.surname = surname;
-		this.teamName = teamName;
-		this.numberOfmember = numberOfmember;
-	}
-	
-	public HokeyTeam(String name,String surname, String teamName, int numberOfmember,int Members, int totalMembers) {
-		
-		this.name = name;
-		this.surname = surname;
-		this.teamName = teamName;
-		this.numberOfmember = numberOfmember;
-		this.Members = Members;
-		this.totalMembers= totalMembers;
-		
-	}
-	
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	
-	public void setSurname(String surname) {
-		this.surname = surname;
-	}
-	
-	public void setTeamName(String teamName) {
-		this.teamName = teamName;
-	}
-	
-	public void setNumberOfMember(int numberOfmember) {
-		this.numberOfmember = numberOfmember;
-	}
-	
-	
-	 public void setMembers(int Members) {
-		 this.Members = Members;
-	 }
-	 
-	 
-	 public String toString() {
-		 return   "name:"+name+"\n"
-				 +"surname:"+surname+"\n"
-				 +"teamName:"+teamName+"\n"
-				 +"numberOfmember:"+numberOfmember+"\n"
-				 +"Members:"+Members+"\n"
-				 +"totalMembers"+totalMembers+"\n";
-	 }
-	 public static void printStaticSum() {
-		 System.out.println("totalMembers:"+totalMembers+"\n");
-	 }
-	 
-     public static void printSum() {
-		 System.out.println("totalMembers: " + totalMembers + "\n");
-	 }
-	 
-	 public void resetValues(String name,String surname, String teamName, int numberOfmember,int Members, int totalMembers) {
-		 
-		    this.name = name;
-			this.surname = surname;
-			this.teamName = teamName;
-			this.numberOfmember = numberOfmember;
-			this.Members = Members;
-			this.totalMembers= totalMembers;
-	 }
-	 
+    private String name;
+    private String surname;
+    private String teamName;
+    private int numberOfmember;
+    private int Members;
+    public static int totalMembers;
+
+    public HokeyTeam() {
+    }
+
+    public HokeyTeam(String name, String surname, String teamName, int numberOfmember) {
+        this.name = name;
+        this.surname = surname;
+        this.teamName = teamName;
+        this.numberOfmember = numberOfmember;
+    }
+
+    public HokeyTeam(String name, String surname, String teamName, int numberOfmember, int Members, int totalMembers) {
+
+        this.name = name;
+        this.surname = surname;
+        this.teamName = teamName;
+        this.numberOfmember = numberOfmember;
+        this.Members = Members;
+        this.totalMembers = totalMembers;
+
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public void setNumberOfMember(int numberOfmember) {
+        this.numberOfmember = numberOfmember;
+    }
+
+
+    public void setMembers(int Members) {
+        this.Members = Members;
+    }
+
+
+    public String toString() {
+        return "name:" + name + "\n"
+                + "surname:" + surname + "\n"
+                + "teamName:" + teamName + "\n"
+                + "numberOfmember:" + numberOfmember + "\n"
+                + "Members:" + Members + "\n"
+                + "totalMembers" + totalMembers + "\n";
+    }
+
+    public static void printStaticSum() {
+        System.out.println("totalMembers:" + totalMembers + "\n");
+    }
+
+    public static void printSum() {
+        System.out.println("totalMembers: " + totalMembers + "\n");
+    }
+
+    public void resetValues(String name, String surname, String teamName, int numberOfmember, int Members, int totalMembers) {
+
+        this.name = name;
+        this.surname = surname;
+        this.teamName = teamName;
+        this.numberOfmember = numberOfmember;
+        this.Members = Members;
+        this.totalMembers = totalMembers;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,32 @@
 
 public class Main {
-	public static void main(String [] args) {
-		
-		 HokeyTeam HalyckiLevy = new HokeyTeam();
-		 
-		 HalyckiLevy.setName("Valentyn");
-		 HalyckiLevy.setSurname("Karas");
-		 HalyckiLevy.setTeamName("Halycki Levy");
-		 HalyckiLevy.setNumberOfMember(13);
-		 HalyckiLevy.setMembers(11);
-		 
-		 System.out.println(HalyckiLevy.toString());
-		 HalyckiLevy.printStaticSum();
-		 HalyckiLevy.printSum();
-		 
-	 
-		 HokeyTeam Harkivci = new HokeyTeam ("Lova","Tonyshev","Harkivci",12);
-		 
-		 System.out.println(Harkivci.toString());
-		 HalyckiLevy.printStaticSum();
-		 HalyckiLevy.printSum();
-		 
-		 
-		 HokeyTeam Lovehokey = new HokeyTeam ("Tom","Tonystar","Lovehokey",14, 15,15);
-		 
-		 System.out.println(Lovehokey.toString());
-		 HalyckiLevy.printStaticSum();
-		 HalyckiLevy.printSum();
-		 
+	public static void main(String[] args) {
+
+		HokeyTeam halyckiLevy = new HokeyTeam();
+
+		halyckiLevy.setName("Valentyn");
+		halyckiLevy.setSurname("Karas");
+		halyckiLevy.setTeamName("Halycki Levy");
+		halyckiLevy.setNumberOfMember(13);
+		halyckiLevy.setMembers(11);
+
+		System.out.println(halyckiLevy.toString());
+		halyckiLevy.printStaticSum();
+		halyckiLevy.printSum();
+
+
+		HokeyTeam harkivci = new HokeyTeam("Lova", "Tonyshev", "Harkivci", 12);
+
+		System.out.println(harkivci.toString());
+		harkivci.printStaticSum();
+		harkivci.printSum();
+
+
+		HokeyTeam lovehokey = new HokeyTeam("Tom", "Tonystar", "Lovehokey", 14, 15, 15);
+
+		System.out.println(lovehokey.toString());
+		lovehokey.printStaticSum();
+		lovehokey.printSum();
+
 	}
 }
